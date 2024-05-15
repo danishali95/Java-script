dcd86d8459acaa03ec063a16721cb8e321f8edf0
DANISH ALI <110814703+danishali95@users.noreply.github.com>
2024-05-15 18:00:41 +0000

    Variables

---
+++ Practice/Variables.js
@@ -0,0 +1,13 @@
+const accountId = 234567
+let accountEmail = "Danishali@gmail.com"
+var accountPassword = "123456789"
+accountCity = "Aligarh"
+
+//accountId = 34 // not allowed
+
+accountEmail = "danis@gmail.com"
+accountPassword = "123"
+accountCity = "Agra"
+console.log([accountEmail,accountPassword,accountCity]);
+
+/*Prefer not to use var because of issue in block scope and functional scope */
