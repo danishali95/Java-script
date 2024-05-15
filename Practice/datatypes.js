775ac234473481d342b749ad87449aa34a9e8575
DANISH ALI <110814703+danishali95@users.noreply.github.com>
2024-05-15 18:17:11 +0000

    datatypes

---
+++ Practice/datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all js code as newer version
+
+
+// alert(3+3) // We are using node.js not browsers
+
+// Number => 2 to power 53
+// bigint 
+// string => "dsnish"
+// boolean => true/false
+// null => standalone value
+// undefined => valuse assign nahi hui hai
+// symbol = unique
+
+//object 
+
+console.log(typeof )
